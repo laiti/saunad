31814364a5f95118c46ea8fc662501837ebb5132
Antti Laiti <antti@laiti.fi>
2023-04-01 17:45:25 +0300

    Update configurator, add jest config, add gitignore

--- app/source/util/configurator.ts
+++ app/source/util/configurator.ts
@@ -11,7 +11,7 @@ import {
 export default class Configurator {
   static async collect(): Promise<Config> {
     const telegramConfig: TelegramConfig = config.get('telegram');
-    const saunadConfig: SaunadConfig = config.get('monkku');
+    const saunadConfig: SaunadConfig = config.get('saunad');
     const messageConfig: MessageConfig = config.get('messages');
     const logLevel: LogLevel = config.get('logLevel');
 
